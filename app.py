28f21511d7f48fc3d26d938ea9fa6e932ccf330f
aeron-j <aeronjay.dale@g.msuii.edu.ph>
2025-09-21 17:42:15 +0800

    made a design for manage college, add college and update college

--- app.py
+++ app.py
@@ -29,3 +29,32 @@ def update_student():
         print("Updating:", student_id, first_name, last_name, course)
         return redirect(url_for("dashboard"))
     return render_template("update_student.html")
+
+@app.route("/manage_college")
+def manage_college():
+    colleges = [
+        {"code": "CENG", "name": "College of Engineering"},
+        {"code": "CBA", "name": "College of Business Administration"},
+    ]
+    total_college = len(colleges)
+    return render_template("manage_college.html", colleges=colleges, total_college=total_college)
+
+@app.route("/add_college", methods=["GET", "POST"])
+def add_college():
+    if request.method == "POST":
+        college_code = request.form["college_code"]
+        college_name = request.form["college_name"]
+        # TODO: Save to database
+        print(college_code, college_name)
+        return redirect(url_for("manage_college"))
+    return render_template("add_college.html")
+
+@app.route("/update_college", methods=["GET", "POST"])
+def update_college():
+    if request.method == "POST":
+        college_code = request.form["college_code"]
+        college_name = request.form["college_name"]
+        # TODO: Update database here
+        print("Updated:", college_code, college_name)
+        return redirect(url_for("manage_college"))
+    return render_template("update_college.html")
